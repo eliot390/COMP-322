1c8431af4ba5f3aacdef164165f639c65a2e74c5
Eliot <eliot.pardo.997@my.csun.edu>
2022-02-25 08:10:55 -0800

    Update Eliot_pardo_02.c
    FIFO & SJF algorithm completion

--- Eliot_pardo_02.c
+++ Eliot_pardo_02.c
@@ -43,11 +43,13 @@ void parameters(){
 }
 
 void print(){
-  printf("\nID\tArrival Time\tTotal Cycles\tStart\tEnd\tTurnaround\n");
-  printf("------------------------------------------------------------------\n");
+  printf("\nID\tArrival\tTotal\tStart\tEnd\tTurnaround\n");
+  printf("--------------------------------------------------\n");
   
   for (int i=0; i<num_processes; i++)  {
-    printf("%d\t%d\t%d\n", table_ptr[i].process_ID, table_ptr[i].arrival, table_ptr[i].total_cycles);
+    printf("%d\t%d\t%d\t%d\t%d\t%d\n", 
+    table_ptr[i].process_ID, table_ptr[i].arrival, table_ptr[i].total_cycles,
+    table_ptr[i].start_time, table_ptr[i].end_time, table_ptr[i].turnaround);
   }  
 
   //if (table_ptr[i].done == 1)
@@ -59,36 +61,136 @@ void FIFO(){
   int count=0;
   int min_time;
   int min_index;
+  int curr_cycle=0;
 
   for (int i = 0; i < num_processes; i++){
     table_ptr[i].done = 0;
   }
 
   // while there are still processes to schedule
-  while (count<num_processes){
+  while (count < num_processes){
     min_time = INT_MAX;
     
     for (int i = 0; i < num_processes; i++){ // for each process not yet scheduled       
       if (table_ptr[i].done == 0){ // check if process has earlier arrival time than current earliest and update 
-        if (table_ptr[i].arrival < num_processes){
-          // new earliest time = table_ptr[i].arrival;
-          // min_index = i;
+        if (table_ptr[i].arrival < min_time){
+          min_time = table_ptr[i].arrival;
+          min_index = i;
         } 
       }
     }
     // set start time, end time, turnaround time, done fields for unscheduled process with earliest arrival time
-    table_ptr[min_index].start_time =
-    table_ptr[min_index].end_time = 
-    table_ptr[min_index].turnaround =  
+    table_ptr[min_index].start_time = max(curr_cycle, table_ptr[min_index].arrival);
+    table_ptr[min_index].end_time = table_ptr[min_index].start_time + table_ptr[min_index].total_cycles;
+    table_ptr[min_index].turnaround = table_ptr[min_index].end_time - table_ptr[min_index].arrival;
     table_ptr[min_index].done = 1;
 
     count++;
+    curr_cycle = table_ptr[min_index].end_time;
+
+    print();
   } 
   
   return;
 }
-void SJF(){}
-void SRT(){}
+
+void SJF(){
+  int count=0;
+  int min_time;
+  int min_index;
+  int curr_cycle=0;
+  int min_flag;
+
+  for (int i = 0; i < num_processes; i++){
+    table_ptr[i].done = 0;
+  }
+
+  while (count < num_processes){
+    min_time = INT_MAX;
+    min_flag=0;
+    
+    for (int i = 0; i < num_processes; i++){
+      if (table_ptr[i].done == 0){ // check if process has shorter cpu time than current shortest and update 
+        if ((table_ptr[i].total_cycles < min_time) && (table_ptr[i].arrival <= curr_cycle)){
+          min_time = table_ptr[i].total_cycles;
+          min_index = i;
+          min_flag = 1;
+        } 
+      }
+    }
+
+    if (min_flag == 0){
+      curr_cycle++;
+    } else {
+      table_ptr[min_index].start_time = max(curr_cycle, table_ptr[min_index].arrival);
+      table_ptr[min_index].end_time = table_ptr[min_index].start_time + table_ptr[min_index].total_cycles;
+      table_ptr[min_index].turnaround = table_ptr[min_index].end_time - table_ptr[min_index].arrival;
+      table_ptr[min_index].done = 1;
+
+      count++;
+      curr_cycle = table_ptr[min_index].end_time;
+    }
+
+    print();
+  } 
+  
+  return;
+}
+
+void SRT(){
+  int count=0;
+  int min_time;
+  int min_index;
+  int curr_cycle=0;
+  int min_flag;
+
+  for (int i = 0; i < num_processes; i++){
+    table_ptr[i].done = 0;
+    table_ptr[i].started = 0;
+    table_ptr[i].total_remain = table_ptr[i].total_cycles;
+  }
+
+  while (count < num_processes){
+    min_time = INT_MAX;
+    min_flag=0;
+    
+    for (int i = 0; i < num_processes; i++){
+      if (table_ptr[i].done == 0){ // check if process has shorter remaining time than current shortest and update 
+        if ((table_ptr[i].total_remain < min_time) && (table_ptr[i].arrival <= curr_cycle)){
+          min_time = table_ptr[i].total_remain;
+          min_index = i;
+          min_flag = 1;
+        } 
+      }
+    }
+
+    if (min_flag == 0){
+      curr_cycle++;
+    } else {
+      if (table_ptr[min_index].started == 0){
+        table_ptr[min_index].started = 1; //??
+        table_ptr[min_index].start_time = max(curr_cycle, table_ptr[min_index].arrival);
+        // table_ptr[min_index].end_time = table_ptr[min_index].start_time;
+        // table_ptr[min_index].turnaround = table_ptr[min_index].end_time - table_ptr[min_index].arrival;
+      } else {
+        table_ptr[min_index].end_time = curr_cycle+1; // table_ptr[min_index].start_time;
+        table_ptr[min_index].turnaround = table_ptr[min_index].end_time - table_ptr[min_index].arrival;
+        table_ptr[min_index].total_remain--;
+      }
+
+      if (table_ptr[min_index].total_remain == 0) //??
+      {
+        table_ptr[min_index].done = 1;
+        count++;
+        curr_cycle++;
+      }
+    }
+    
+    print();
+  } 
+  
+  return;
+}
 
 void quit(){       
   free(table_ptr);
